9d5fdc7d1d732b2edc46dbd5254f014cb7a28f15
mulinatti <gabrielmulinari2002@gmail.com>
2024-10-23 15:43:36 -0300

    add: not found page

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,9 @@ function App() {
               <Route path="/" element={<Sidebar />}>
                 <Route path="/:id" element={<Board />} />
               </Route>
+              <Route path="*" element={<h2 className="text-2xl lg:text-3xl p-5 lg:p-0 font-bold text-center space-y-2 w-full h-screen flex flex-col justify-center">
+                Page Not Foud
+              </h2>}/>
             </Routes>
           </BrowserRouter>
         </BoardsProvider>
